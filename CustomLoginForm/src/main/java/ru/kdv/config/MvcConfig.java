bf7a0b3c1e3dcb754bd027f2e40d4c091f65f679
KDanila <concista@gmail.com>
2020-03-06 11:38:19 +0300

    Add spring boot.

---
+++ CustomLoginForm/src/main/java/ru/kdv/config/MvcConfig.java
@@ -0,0 +1,27 @@
+package ru.kdv.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class MvcConfig {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+
+}
---
+++ CustomLoginForm/src/main/java/ru/kdv/constants/SecurityConfigConstants.java
@@ -0,0 +1,12 @@
+package ru.kdv.constants;
+
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SecurityConfigConstants {
+    public static final String EMPLOYEE = "EMPLOYEE";
+    public static final String MANAGER = "MANAGER";
+    public static final String ADMIN = "ADMIN";
+}
