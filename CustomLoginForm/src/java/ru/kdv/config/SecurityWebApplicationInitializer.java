386c25785297e64732bff372b31a3461b275d677
KDanila <concista@gmail.com>
2020-03-14 17:47:51 +0300

    1. Add logout. 2. Check CSRF. 3. Add spring-thymeleaf authentication.

--- CustomLoginForm/src/java/ru/kdv/config/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package ru.kdv.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
--- CustomLoginForm/src/main/java/ru/kdv/config/SecurityConfig.java
+++ CustomLoginForm/src/main/java/ru/kdv/config/SecurityConfig.java
@@ -34,6 +34,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/showLoginForm")
                 .loginProcessingUrl("/authenticateUser")
+                .permitAll()
+                .and()
+                .logout()
                 .permitAll();
 
     }
