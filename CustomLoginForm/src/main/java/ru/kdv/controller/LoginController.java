30e3b155a458dcbcb819d44cb02b799d5969fdc6
Danila.V.Kuzmin <danila.v.kuzmin@rt.ru>
2020-03-17 10:12:29 +0300

    Add role visible section.

--- CustomLoginForm/src/main/java/ru/kdv/controller/LoginController.java
+++ CustomLoginForm/src/main/java/ru/kdv/controller/LoginController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
     @GetMapping("/showLoginForm")
-    public String showLoginForm(){
+    public String showLoginForm() {
         return "bootstrap-login";
+    }
+
+    @GetMapping("/access-denied")
+    public String showDeniedForm() {
+        return "access-denied";
     }
 }
